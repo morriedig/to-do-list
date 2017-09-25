010b81d2eff32b614f3c078e8240dea6c526ffb3
Morrie <morriedog@hotmail.com>
2017-09-25 20:29:37 +0800

    finish the class part

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,43 @@
 class ListsController < ApplicationController
+	before_action :set_list, :only => [:show, :edit, :update, :destroy]
 	def index
 		@lists = List.all
 	end
+
+	def new
+		@list = List.new
+	end
+
+	def create
+		@list = List.new(list_params)
+		if @list.save
+			redirect_to lists_url
+		else
+			render :new
+		end
+	end
+
+
+	def update
+	  @list.update_attributes(list_params)
+
+	  redirect_to list_path(@list)
+	end
+
+	def destroy
+	  @list.destroy
+
+	  redirect_to lists_url
+	end
+
+	private
+
+	def list_params
+		params.require(:list).permit(:title, :date,:due_date , :description, :detail)
+	end
+
+	def set_list
+		@list = List.find(params[:id])
+	end
+
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,3 @@
 class List < ApplicationRecord
+	validates_presence_of :title, :date, :due_date, :description
 end
